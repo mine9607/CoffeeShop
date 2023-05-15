359332b4155efb7b8d498243fd8522d681766890
mine9607 <81383389+mine9607@users.noreply.github.com>
2023-05-14 22:33:14 -0500

    Added some JS to create a carousel

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelectorAll(".carousel").forEach((carousel) => {
+  const items = carousel.querySelectorAll(".carousel__item");
+  const buttonsHtml = Array.from(items, () => {
+    return `<span class="carousel__button"></span>`;
+  });
+
+  console.log(buttonsHtml);
+});
